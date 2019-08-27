fbe07bc74bf66bea2c39ca08820af51770945f41
Anthony HOCQUET <anthony.hocquet@younited-credit.fr>
2019-08-27 17:51:18 +0200

    Migrate NEventStore sample to netcore 2.2

--- src/NEventStore/AggregateSource.NEventStore.Tests/Framework/AggregateRootEntityStub.cs
+++ src/NEventStore/AggregateSource.NEventStore.Tests/Framework/AggregateRootEntityStub.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using AggregateSource.Content.ExplicitRouting;
 
 namespace AggregateSource.NEventStore.Framework
 {
--- src/NEventStore/AggregateSource.NEventStore.Tests/Framework/Snapshots/SnapshotableAggregateRootEntityStub.cs
+++ src/NEventStore/AggregateSource.NEventStore.Tests/Framework/Snapshots/SnapshotableAggregateRootEntityStub.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using AggregateSource.Content.ExplicitRouting;
 
 namespace AggregateSource.NEventStore.Framework.Snapshots
 {
